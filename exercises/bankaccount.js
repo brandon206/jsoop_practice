052c7ecbe66d7f7163ce41a713df37b208dea13f
Brandon Park <brandonpark206@gmail.com>
2018-11-24 18:56:20 -0800

    finished creating bank account functinality

--- exercises/bankaccount.js
+++ exercises/bankaccount.js
@@ -2,15 +2,30 @@
 
 class Account{
 	constructor(){
+		this.amountOfMoney = null;
+		this.withdrawAmount = null;
 		//store the amount of money in the account
 	}
-	add(  ){
+	add( amount ){
+		if(typeof amount === "number"){
+			let updatedAmountOfMoney = this.amountOfMoney += amount;
+			return updatedAmountOfMoney;
+		}
 		//add money to the amount stored in the account
 		//takes in an amount
 		//adds it to the existing amount
 		//returns the new amount in the account
 	}
 	remove( amount ){
+		if(amount > this.amountOfMoney){
+			this.withdrawAmount = this.amountOfMoney;
+			this.amountOfMoney -= this.withdrawAmount;
+		}
+		else {
+			this.withdrawAmount = amount;
+			this.amountOfMoney -= this.withdrawAmount;
+		}
+		return this.withdrawAmount;
 		//removes funds from an account
 		//check if the amount to withdraw is more than the account
 		//if more, only withdraw the amount in the account, not more
@@ -18,6 +33,7 @@ class Account{
 		//return the amount actually withdrawn
 	}
 	getAmount(){
+		return this.amountOfMoney;
 		//returns the amount in the account
 	}
 }
