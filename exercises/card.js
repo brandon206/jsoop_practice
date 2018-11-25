153a0a0ea98467a7956ef2d4383c5c1999676c99
Brandon Park <brandonpark206@gmail.com>
2018-11-24 21:19:24 -0800

    finished all tests including card and deck test

--- exercises/card.js
+++ exercises/card.js
@@ -14,10 +14,14 @@ class Card{
 
 class Deck{
 	constructor(){
+		this.set = [];
 		//takes in nothing
 		//make storage for your card objects
 	}
 	addCard( suite, faceValue ){
+		let newCard = new Card (suite,faceValue);
+		this.set.unshift(newCard);
+		return this.set.length;
 		//adds a card to the deck
 		//takes in a string suite and faceValue
 		//makes a new card Object from the Card template
@@ -25,16 +29,36 @@ class Deck{
 		//returns the amount of cards currently stored
 	}
 	shuffle(){
+		let copyArray = this.set.slice(0);
+		let shuffledArray = [];
+
+		for(let i = 0; i < copyArray.length; i++){
+			let randomIndex = Math.floor(Math.random * this.set.length);
+			shuffledArray.push(copyArray[randomIndex]);
+			copyArray.splice(copyArray[randomIndex],1);
+		}
 		//reorders the cards in the storage array in a random order
 		//takes in nothing
 		//returns nothing
 	}
 	getCardCount(){
+		return this.set.length;
 		//gets the current amount of cards stored
 		//takes in nothing
 		//returns the amount of cards stored in the array in the constructor
 	}
-	dealCards(  ){
+
+	dealCards( numberToDeal ){
+		let copyArray = this.set.slice(0);
+		let cardsRemoved = this.set.splice(0,numberToDeal);
+
+		if(cardsRemoved > this.set.length){
+			return;
+		}
+		else{
+			return cardsRemoved;
+		}
+		
 		//deals out a number of cards
 		//takes in the number of cards to deal
 		//removes that many cards from the deck
